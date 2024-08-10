dc65e30361ac60aba4f4cb994360a01d7017b1ca
Caleb <food.please.dev@gmail.com>
2024-08-10 09:48:09 -0700

    Add tilesets, touch camera, and a test layout

--- src/main.gd
+++ src/main.gd
@@ -1,8 +1,13 @@
 extends Node
 
+@onready var camera: = $TouchCamera as TouchCamera
+@onready var terrain: = $World/Terrain as TileMapLayer
 
 func _ready() -> void:
-	# Game.Start()
-	# Awesomeness.Add(9999)
-	# Jam.Complete()
-	pass
+	# Setup the camera, its starting position, and its boundaries.
+	var viewport_dimensions: = get_viewport().get_visible_rect().size
+	var map_dimensions: = terrain.get_used_rect()
+	camera.boundary = Rect2(
+		map_dimensions.position * terrain.tile_set.tile_size + Vector2i(viewport_dimensions/2.0),
+		map_dimensions.size * terrain.tile_set.tile_size - Vector2i(viewport_dimensions)
+	)
---
+++ src/player/touch_camera.gd
@@ -0,0 +1,43 @@
+class_name TouchCamera extends Node2D
+
+# How far the cursor must drag before the event is recognized as a drag (vs a
+#	touch).
+@export var DRAG_THRESHOLD: = 10.0
+@export var CLICK_THRESHOLD: = 20.0
+
+# Keep track of the distance dragged before allowing the touch event through.
+var _drag_distance: = 0.0
+var _is_dragged: = false
+
+@export var boundary: = Rect2():
+	set(value):
+		boundary = value
+		clamp_to_boundary()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventScreenDrag and _is_dragged:
+		_drag_distance += event.relative.length()
+		if _drag_distance >= DRAG_THRESHOLD:
+			position -= event.relative
+			clamp_to_boundary()
+		
+		get_viewport().set_input_as_handled()
+	
+	if event.is_action_pressed("touch"):
+		_is_dragged = true
+		_drag_distance = 0.0
+	
+	elif event.is_action_released("touch"):
+		_is_dragged = false
+		
+		# If the cursor has been dragged far enough, act like it was meant for the camera and do 
+		# not allow other objects to act on it.
+		if _drag_distance >= CLICK_THRESHOLD:
+			get_viewport().set_input_as_handled()
+
+
+func clamp_to_boundary() -> void:
+	if not boundary.has_point(global_position):
+		global_position.x = clampf(global_position.x, boundary.position.x, boundary.end.x)
+		global_position.y = clampf(global_position.y, boundary.position.y, boundary.end.y)
